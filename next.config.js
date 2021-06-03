eac2c6886636040ecfd2620ba42b3e5ad90a0878
Damián Ricobelli <damian.ricobelli@outlook.es>
2021-06-03 13:08:07 -0300

    Add api keys of yelp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   env: {
     REACT_APP_GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
-    API_KEY_ABSTRACT: process.env.API_KEY_ABSTRACT
+    API_KEY_ABSTRACT: process.env.API_KEY_ABSTRACT,
+    YELP_CLIENT_ID: process.env.YELP_CLIENT_ID,
+    YELP_API_KEY: process.env.YELP_API_KEY
   },
   future: {
     webpack5: true
