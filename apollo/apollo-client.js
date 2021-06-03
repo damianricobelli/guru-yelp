cf7b56171d0d142bd5eb73fe2d7f2aa52f770bd9
Damián Ricobelli <damian.ricobelli@outlook.es>
2021-06-03 14:41:45 -0300

    Update apollo client with info offered by Jorge

--- apollo/apollo-client.js
+++ apollo/apollo-client.js
@@ -1,21 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 
-console.log(process.env.YELP_API_KEY)
+const httpLink = createHttpLink({
+  uri: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql"
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.YELP_API_KEY
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+      "Accept-Language": "en-US"
+    }
+  }
+})
 
 const client = new ApolloClient({
-  uri: "https://api.yelp.com/v3/graphql",
-  cache: new InMemoryCache(),
-  request: (operation) => {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${process.env.YELP_API_KEY}`,
-        "Content-Type": "application/graphql"
-      }
-    })
-  },
-  fetchOptions: {
-    mode: "no-cors"
-  }
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 })
 
 export default client
